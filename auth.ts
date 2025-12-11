c16ff831df295c8856dfe10dccaab4c62e5eb8ef
Paul Boluwatife <paulboluwatife377@gmail.com>
2025-12-11 13:24:00 +0100

    added google auth

--- auth.ts
+++ auth.ts
@@ -36,6 +36,7 @@
 
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@/app/generated/prisma";
 
@@ -47,6 +48,11 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     GitHub({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
+    }),
+
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
   callbacks: {
